5559731a1541370ba07f40c0eff6394bd5e22f25
serega-miha <serega.miha@gmail.com>
2024-02-05 16:59:01 +0400

    check change status

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/header/header';
 import HomePage from './pages/homePage/HomePage';
 import HeroPage from './pages/heroPage/HeroPage';
 import Page404 from './components/404/404';
-import Tasks from './tasks';
+
 
 function App() {
   return ( 
@@ -15,7 +15,7 @@ function App() {
         <Route path={'/hero/:id'} element={<HeroPage/>}/>
         <Route path={'*'} element={<Page404/>}/>
       </Routes>
-      <Tasks/>
+
     </div>
   ); 
 }
--- src/components/404/404.js
+++ src/components/404/404.js
@@ -1,10 +1,36 @@
-import { Link } from "react-router-dom"
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom"
+
 
 const Page404 = () => {
+    const navigate = useNavigate();
+
+
+
+
+    let str = 'qwertyqq'
+
+    let arr = str.split('')
+    function uniqueArray4(a) {
+        return [...new Set(a)];
+      }
+
+    function isPangram(string) {
+        let arr = string.split('')
+        let newArr = [...new Set(arr)]
+        if (arr.length > newArr.length){
+            return false
+        } else {
+            return true
+        }
+    }
+
+    console.log(isPangram('Hello world'));
+
     return (
         <>
-        <h2>Упс!!!! что-то пошло не так, давайте попробуем еще раз</h2>
-        <h2><Link to=''>Нажмите пожалуйста сюда</Link> или на главную страничку</h2>
+            <h2>Упс!!!! что-то пошло не так, давайте попробуем еще раз</h2>
+            <h2><Link to=''>Нажмите пожалуйста сюда</Link> или на главную страничку</h2>
         </>
     )
 }
